3d0467cd025a0cb9a84df18ad14552a153784671
ASWATHI CV <112601873+Aswathicv26@users.noreply.github.com>
2022-09-05 12:14:29 +0530

    Update streamlit.app.py
    Added images

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents New healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣🥑🍞Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 🐔Kale, spinach &Rockect Smoothie🥗 🐔')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, spinach &Rockect Smoothie')
 streamlit.text('🐔Hard -Boilded Free-ange Egg')
+streamlit.text('🥑🍞Avagado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
